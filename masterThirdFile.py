0a78f9fe9e3f602ad2e510e9150b2a6aca5bb825
PuruMukherjee <61185540+PuruMukherjee@users.noreply.github.com>
2024-09-27 18:54:24 +0530

    Update masterThirdFile.py
    Added change in 18:52

--- masterThirdFile.py
+++ masterThirdFile.py
@@ -2,3 +2,9 @@ print("Hello World!")
 print("This change is made in the remote repo")
 # This change is made in the repote repo and this change at this instance of time is not in the local
 print('This is a change to the remote repo is 18:44')
+
+# Want to create a conflict
+# I am going to make a change in this file in the remote at 18:52
+# I will also make a change in the same file in the working directory  and try to pull from remote
+# This will create a conflict and I will resolve the issue
+print("This change was made in remote at 18:52")
